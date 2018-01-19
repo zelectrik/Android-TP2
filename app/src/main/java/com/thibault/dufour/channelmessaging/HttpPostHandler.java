d87d298f7e4623bbf186fd0b35433c41959a13d3
Thibault DUFOUR <dufourth@iut-acy.local>
2018-01-19 16:45:41 +0100

    Avancement

--- app/src/main/java/com/thibault/dufour/channelmessaging/HttpPostHandler.java
+++ app/src/main/java/com/thibault/dufour/channelmessaging/HttpPostHandler.java
@@ -1,23 +1,105 @@
 package com.thibault.dufour.channelmessaging;
 
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by dufourth on 19/01/2018.
  */
-public class HttpPostHandler extends AsyncTask<String,String,String> {
+public class HttpPostHandler extends AsyncTask<String, String, String> {
+
     private ArrayList<OnDownloadListener> list_listener = new ArrayList<OnDownloadListener>();
 
-    public void addOnDownloadListener(OnDownloadListener listener)
-    {
+    public void addOnDownloadListener(OnDownloadListener listener) {
         list_listener.add(listener);
     }
 
-
     @Override
     protected String doInBackground(String... params) {
-        return null;
+        HashMap param = new HashMap();
+        param.put("username",params[0]);
+        param.put("password",params[1]);
+        String retour = performPostCall("http://raphaelbischof.fr/messaging/?function=connect",param);
+        return retour;
     }
+
+    @Override
+    protected void onPostExecute(String result) {
+        for (OnDownloadListener oneListener : list_listener)
+        {
+            Gson gson = new Gson();
+            Object test = gson.fromJson(result,Object.class);
+            oneListener.onDownloadComplete(test.toString());
+        }
+    }
+
+    public String performPostCall(String requestURL, HashMap<String, String> postDataParams) {
+        URL url;
+        String response = "";
+        try {
+            url = new URL(requestURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            writer.write(getPostDataString(postDataParams));
+            writer.flush();
+            writer.close();
+            os.close();
+            int responseCode= conn.getResponseCode();
+            if(responseCode == HttpsURLConnection.HTTP_OK) {
+                String line;
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while ((line = br.readLine()) != null) {
+                    response += line;
+                }
+            } else {
+                response= "";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException
+    {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first) first = false;
+            else result.append("&");
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+        return result.toString();
+    }
+
+
 }
--- app/src/main/java/com/thibault/dufour/channelmessaging/LoginActivity.java
+++ app/src/main/java/com/thibault/dufour/channelmessaging/LoginActivity.java
@@ -2,11 +2,14 @@ package com.thibault.dufour.channelmessaging;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements
+        View.OnClickListener{
     private TextView txt_id,txt_mdp;
     private EditText etxt_id, etxt_mdp;
     private Button btn_valider;
@@ -21,5 +24,28 @@ public class LoginActivity extends AppCompatActivity {
         etxt_id = (EditText) findViewById(R.id.editText_identifiant);
         etxt_mdp = (EditText) findViewById(R.id.editText_mdp);
         btn_valider = (Button) findViewById(R.id.button_valider);
+        btn_valider.setOnClickListener(this);
+        String result;
+
+
     }
+
+    @Override
+    public void onClick(View v) {
+        HttpPostHandler connection = (HttpPostHandler) new HttpPostHandler().execute(etxt_id.getText().toString(),etxt_mdp.getText().toString());
+
+        connection.addOnDownloadListener(new OnDownloadListener() {
+            @Override
+            public void onDownloadComplete(String downloadedContent) {
+                Toast.makeText(getApplicationContext(),downloadedContent,Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDownloadError(String error) {
+
+            }
+        });
+    }
+
+
 }
